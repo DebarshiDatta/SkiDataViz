9243ed3e9b427c13043ad4326c614448c0246250
Andrew Yale <a.yale9@gmail.com>
2013-12-17 14:23:30 -0500

    add comments

--- get_resort_urls.py
+++ get_resort_urls.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
-
+# open state urls list
 f = open('state_urls.txt', 'r')
 urls = []
 
@@ -10,27 +10,41 @@ for line in f:
 
 resorts = []
 
+# for each state
 for url in urls:
+	# get page
 	r = requests.get(url)
 
+	# get text from page object
 	data = r.text
 
+	# create soup object
 	soup = BeautifulSoup(data)
 
+	# flag tells you if you are resort links yet
 	flag = 0
+	# front of the urls
 	front = 'http://www.skiresortsnow.com'
 
+	# for each link in the page
 	for link in soup.find_all('a'):
+		# if you are past the resort links
 		if link.get('href') == '/ski-resorts/usa/alaska/':
+			# leave loop
 			break	
+		# if you are at the resorts
 		elif flag:
+			# add to the list or resort urls
 			resorts.append(front + link.get('href'))
+		# if at the last link before resort urls set flag
 		elif link.get('href') == 'http://www.abc.net.au/melbourne/news/200310/s961541.htm':
 			flag = 1
 
 
+# open new file
 g = open('resort_urls.txt', 'w')
 
+# add all the resorts to file
 for resort in resorts:
 	g.write(resort + '\n')
 
